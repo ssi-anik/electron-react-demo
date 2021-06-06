7964c47876aa037c4f2d66a8773c66c079339e66
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-06-06 19:53:03 +0600

    full app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactWeather, {useOpenWeather} from 'react-open-weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const {data, isLoading, errorMessage} = useOpenWeather({
+        key: 'a93f5ae9362313665d7333a5494ff13e', // just the dummy data. will be removed
+        lat: '13.7495868', lon: '90.3905', lang: 'en', unit: 'metric', // values are (metric, standard, imperial)
+    });
+    return (<div className = "App">
+        <ReactWeather
+            isLoading = {isLoading}
+            errorMessage = {errorMessage}
+            data = {data}
+            lang = "en"
+            locationLabel = "Nairobi"
+            unitsLabels = {{temperature: 'C', windSpeed: 'Km/h'}}
+            showForecast
+        />
+    </div>);
 }
 
 export default App;
