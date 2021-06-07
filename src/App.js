196e02a44872c853419eaf0e2a5d1bd0ed5f422f
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-06-07 10:07:59 +0600

    mac + linux distributable added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App () {
                 target = "_blank"
                 rel = "noopener noreferrer"
             >
-                Learn React
+                Trying electron
             </a>
         </header>
     </div>);
