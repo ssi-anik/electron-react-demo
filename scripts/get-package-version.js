352a4b3fc567f87822bf5530ba801856f5dbbf20
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-06-09 13:42:09 +0600

    get package version code change

--- scripts/get-package-version.js
+++ scripts/get-package-version.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const package_version = require(path.join(__dirname, '../package.json')).version;
-const build_number = process.argv.slice(2)[0] || '';
+const path = require('path');
+console.log(require(path.join(__dirname, '../package.json')));
 
-const output = package_version + (build_number ? '-build-' + build_number : '');
-console.log(output);
+//const package_version = require(path.join(__dirname, '../package.json')).version;
+//const build_number = process.argv.slice(2)[0] || '';
+//
+//const output = package_version + (build_number ? '-build-' + build_number : '');
+//console.log(output);
